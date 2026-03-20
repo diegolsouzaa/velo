b60dd9f90c07217fc586ac170502be418381444e
diegolsouzaa <diegolsouzaa@gmail.com>
2026-03-19 21:48:40 -0300

    Change scenario name

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('webspp deve estar online', async ({ page }) => {
+test('webapp deve estar online', async ({ page }) => {
   await page.goto('https://localhost:5173/');
 
   // Expect a title "to contain" a substring.
